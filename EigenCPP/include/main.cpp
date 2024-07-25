f0b6a98998010c35d4121d02a8f6820a3b2e3c6d
ahmed <ahmahmoud@ucdavis.edu>
2024-07-24 23:33:17 -0400

    code

---
+++ EigenCPP/include/main.cpp
@@ -0,0 +1,33 @@
+#include "Eigen/SparseCholesky"
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    int n = 10;
+
+    Eigen::SparseMatrix<float> A(n, n);
+
+    std::vector<Eigen::Triplet<float>> triplets;
+    for (uint32_t v = 0; v < n; ++v) {
+        triplets.push_back(Eigen::Triplet<float>(v, v, 0.1f));
+    }
+
+    A.setFromTriplets(triplets.begin(), triplets.end());
+
+    Eigen::SimplicialLDLT<Eigen::SparseMatrix<float>> solver;
+
+    solver.compute(A);
+
+    Eigen::VectorXf x(n);
+
+
+    Eigen::VectorXf b(n);
+    b.setOnes();
+
+    x = solver.solve(b);
+
+    std::cout << x << "\n";
+
+    return 0;
+}
